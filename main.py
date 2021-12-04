73a46bf6fe1873ed394f3c60af9eb36dd6c1dfa3
Mac <Sophie@Ramans-MacBook-Pro.local>
2021-12-04 20:10:57 +0000

    multiply function updated

--- main.py
+++ main.py
@@ -1,7 +1,3 @@
-import pandas as pd
-import numpy as np
-
-
 
 def add(*args):
 
@@ -9,10 +5,18 @@ def add(*args):
     for n in args:
 
         count +=n
-    print(count)
+    print(f"Add results: {count}")
     return count
 
 add(2,3,4,5,6,7)
 
 
+def multiply(*args):
+    count= 1
+    for n in args:
+        count*=n
+    print(f"Multiply results: {count}")
 
+    return count
+
+multiply(2,3,4,5,6,7)
