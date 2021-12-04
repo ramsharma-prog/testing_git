411ca68204ae61bb441c3ad599b7a02531be7c2e
Mac <Sophie@Ramans-MacBook-Pro.local>
2021-12-04 20:40:26 +0000

    Updated add & multiply fun again

--- main.py
+++ main.py
@@ -1,18 +1,25 @@
-import pandas as pd
-import numpy as np
-
-
-
 def add(*args):
 
     count = 0
     for n in args:
 
         count +=n
-    print(count)
+    print(f"Add function result: {count}")
     return count
 
+
+
+def multiply(*args):
+
+    count = 1
+    for n in args:
+
+        count *=n
+    print(f"Multiply function result: {count}")
+    return count
+
+
+
+
 add(2,3,4,5,6,7)
-
-
-
+multiply(2,3,4,5,6,7)
