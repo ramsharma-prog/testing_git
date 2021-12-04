401c8efd4e8ce16f9b0da60fa98ddca98b57f0ab
Mac <Sophie@Ramans-MacBook-Pro.local>
2021-12-04 22:38:38 +0000

    Add calculate function

--- main.py
+++ main.py
@@ -1,24 +1,25 @@
-
 def add(*args):
-
+    """ Add function"""
     count = 0
     for n in args:
-
-        count +=n
-
+        count += n
     print(f"Add function result: {count}")
-
     return count
 
 
-
 def multiply(*args):
-
+    """ Multiply function"""
     count = 1
     for n in args:
-
-        count *=n
+        count *= n
     print(f"Multiply function result: {count}")
     return count
 
 
+def calculate(fun, *args):
+    """ Takes functions"""
+    return fun(*args)
+
+
+# Call calculate function #
+calculate(add, 1, 2, 3, 4, 5, )
